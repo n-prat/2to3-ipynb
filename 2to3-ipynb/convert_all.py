96128d94ded1d028bd89b8ec943c6b38e1e85ac6
Nathan Prat <nathan.prat@gmail.com>
2015-10-31 15:23:21 +0100

    Use argparse to check arguments
    Also improve logging

--- 2to3-ipynb/convert_all.py
+++ 2to3-ipynb/convert_all.py
@@ -3,32 +3,40 @@
 import os
 import convert_ipynb as ipy
 import logging
+import argparse
 
 def main(argv):
-    if len(argv) != 2:        
-        print("Usage: {} directory".format(argv[0]))                
-        return 1
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("directory")
+    parser.add_argument("--log", dest="logLevel", 
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help="Set the logging level")
+
+    args = parser.parse_args()
+
+    if( args.logLevel ):
+        logging.basicConfig(level=getattr(logging, args.logLevel))
 
     path2to3,cmd2to3 = ipy.find_2to3()
 
-    for subdir, dirs, files in os.walk(argv[1]):
+    for subdir, dirs, files in os.walk(args.directory):
         for file in files:
             filename, file_extension = os.path.splitext(file)
             if file_extension == ".py":
                 full_path = os.path.join(subdir, file)
-                print("found python file:",full_path)
+                logging.info("found python file: %s",full_path)
                 # TODO multithread / Popen
                 ipy.convert_py_file(full_path,path2to3,cmd2to3)
             elif file_extension == ".ipynb":
                 full_path = os.path.join(subdir, file)
-                print("found IPython notebook:",full_path)
+                logging.info("found IPython notebook: %s",full_path)
                 ipy.convert_ipynb_file(full_path,path2to3,cmd2to3)
             else:
-                logging.info("ignoring:",filename,file_extension)
+                logging.info("ignoring: %s%s",filename,file_extension)
 
     print("\n *************************** \n")
-    print("Manual conversions could still be needed")
-    print("Check notes README.md")    
+    print(" Manual conversions could still be needed")
+    print(" Check notes in README.md")    
     print("\n *************************** \n")
 
     return 0
